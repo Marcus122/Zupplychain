e3d9cebace20261855c2279702d5651051417e88
Tom Nuttall <tom.nuttall@weaveability.com>
2015-03-06 15:47:47 +0000

    fixed tile image

--- js/main.js
+++ js/main.js
@@ -5,7 +5,16 @@ require(["jquery"], function($) {
     
     init();
     
+    function onPage(pagename){
+        return ($(location).attr("pathname").indexOf(pagename) != -1);
+    }
+    
     function init(){
+        
+        if (onPage("search-results")){
+            //require the page specific module for example;
+
+        }
         
     }
 
