bcb972192d2d3b6758cbde4ee8bec77f0c10b182
Tom Nuttall <tom.nuttall@weaveability.com>
2015-03-09 11:56:37 +0000

    Added JS to search results map

---
+++ js/components/search-results-map.js
@@ -0,0 +1,97 @@
+define(['async!https://maps.googleapis.com/maps/api/js' , "jquery"], function (GM, $) {
+
+    
+    function Class(data) {
+    
+        var MAP_ELEM_ID = "map-container"
+        var RESULT_INFO_ELEM_SELECTOR = "#search-results-info";
+        var markers = []
+        var selectedMarkerIndex;
+        
+        function initialize() {
+          var loc1 = new google.maps.LatLng(data[0].latitude, data[0].longitude);
+          var mapOptions = {
+            center: loc1,
+            zoom: 14
+          };
+          var map = new google.maps.Map( document.getElementById(MAP_ELEM_ID), mapOptions);
+          
+          var normalIcon = {
+              url  : "/images/map-icon.png"
+          }
+          
+          var highlightIcon = {
+            url  : "/images/map-icon-highlight.png"
+          }
+          
+          
+          var markerOptions = {
+              icon: normalIcon,
+              map:map,
+              animation:'DROP'
+          }
+
+          var lim = data.length;
+          for (var i =0;i<lim;i++) {
+              var position = new google.maps.LatLng(data[i].latitude, data[i].longitude);
+              
+              var thisMarkerOptions = $.extend(markerOptions, {
+                title : data[i].name,
+                position : position,
+                icon : normalIcon
+              });
+              if (i==0) {
+                  thisMarkerOptions.icon = highlightIcon;
+              }
+              
+              var thisMarker = new google.maps.Marker(thisMarkerOptions);
+              setMarkerOnClick(map, thisMarker, markers, data[i], highlightIcon, normalIcon);
+              thisMarker.index = i;
+              markers.push(thisMarker);
+          }
+          
+        }
+        
+        function select(index) {
+            //activate the marker,
+            //load the data,
+        }
+        
+        //TODO : move this to select and add a call.
+        function setMarkerOnClick(map, marker, markers, data, highlightIcon, normalIcon) {
+            google.maps.event.addListener(marker, 'click', function() {
+                map.panTo(marker.getPosition());
+                loadWarehouseData(data);
+                unsetMarkers(markers, normalIcon);
+                marker.setIcon(highlightIcon);
+                
+            });
+            
+            function unsetMarkers(markers, normalIcon){
+                var lim = markers.length;
+                for (var i= 0;i<lim;i++){
+                    markers[i].setIcon(normalIcon);
+                }
+            }
+            
+        }
+        
+        function loadWarehouseData(data) {
+            var resultsElem = $(RESULT_INFO_ELEM_SELECTOR);
+            resultsElem.find('.js-name').html(data.name);
+            resultsElem.find('.js-address').html(data.address);
+            resultsElem.find('.js-name').html(data.name);
+            resultsElem.find('.js-image').addClass("rotateY90");
+            setTimeout(function(){resultsElem.find('.js-image').prop("src", data.imageURL).removeClass("rotateY90");}, 300 );
+        }
+
+        $(function() {
+            initialize();
+        });
+
+    }
+    
+    return Class;
+    
+    
+});
--- js/main.js
+++ js/main.js
@@ -12,7 +12,30 @@ require(["jquery"], function($) {
     function init(){
         
         if (onPage("search-results")){
-            //require the page specific module for example;
+            require(["components/search-results-map"],function(ResultsMap){
+                var testData = [
+                {
+                    latitude : 53.593, 
+                    longitude: -2.298,
+                    name: "Warehouse 1", 
+                    address: "123 Bury New Road, BL1 1HG", 
+                    rating: 4, 
+                    imageURL : "/images/warehouse-thumb.jpg"
+                },
+                {
+                    latitude : 53.592,
+                    longitude:-2.305,
+                    name:"Warehouse 2",
+                    address: "88 Bolton Road, BL8 8TH", 
+                    rating: 2, 
+                    imageURL : "/images/warehouse-thumb2.jpg"
+                    
+                }
+                ]
+                
+                var resultsMap = new ResultsMap(testData);
+                
+            });
         }
         
     }
