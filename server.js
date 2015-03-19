15c9ca80f6e300166dd18445359f5ea24d581680
Tom Nuttall <tom.nuttall@weaveability.com>
2015-03-19 12:03:42 +0000

    Everything now uses EJS, few changes to server too.

--- server.js
+++ server.js
@@ -18,14 +18,27 @@ app.get('/test', function (req,res) {
     console.log("testing render with EJS");
     res.render("index");
 });
-app.get('*', function(req, res) {
-    console.log("connection: GET " + req.path);
-    if (req.path.indexOf(".html") == -1) {
-        var path =  req.path + '.html';
-        res.redirect(path);
+
+app.get('/provider-registration', function(req,res){
+    res.render("provider-registration");
+});
+app.get('/provider-registration/:step', function (req,res) {
+    if (req.params.step == 0) {
+        res.render("provider-registration");
     } else {
-        res.redirect('/');
+        res.render("provider-registration" + "-" + req.params.step);
     }
+});
+
+
+app.get('*', function(req, res) {
+    try {
+        res.render(req.path.substring(1, req.path.length)); //strip leading '/'
+    }
+    catch (ex){
+        res.status(404).send("404 : page not found");
+    }
+        
 });
 app.post('*', function(req, res) {
     console.log("connection: POST " + req.path);
