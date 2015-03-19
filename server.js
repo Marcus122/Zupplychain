5cd7d5a801ed42bfadaeded7f3501872a9cf4cdd
Marcus Atty <marcus.atty@gmail.com>
2015-03-19 10:33:19 +0000

    Logging and body parser

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var morgan = require('morgan')
+
 app.set('view engine', 'ejs');
+// parse application/x-www-form-urlencoded 
+app.use(bodyParser.urlencoded({ extended: false }));
+ 
+// parse application/json 
+app.use(bodyParser.json());
 
 app.use(express.static(__dirname));
+
+app.use(morgan('combined'));
+
 app.get('/test', function (req,res) {
     console.log("testing render with EJS");
     res.render("index");
