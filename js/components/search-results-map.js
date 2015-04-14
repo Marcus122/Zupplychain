1acf17aa911538f9ea9c0db3dff784724d22e3db
Tom Nuttall <tom.nuttall@weaveability.com>
2015-04-14 13:50:05 +0100

    loom update

--- js/components/search-results-map.js
+++ js/components/search-results-map.js
@@ -1,22 +1,14 @@
 define(['async!https://maps.googleapis.com/maps/api/js' , "jquery"], function (GM, $) {
 
     
-    function Class(data) {
+    function Class() {
     
         var MAP_ELEM_ID = "map-container"
         var RESULT_INFO_ELEM_SELECTOR = "#search-results-info";
         var markers = []
-        var selectedMarkerIndex;
-        
-        function initialize() {
-          var loc1 = new google.maps.LatLng(data[0].latitude, data[0].longitude);
-          var mapOptions = {
-            center: loc1,
-            zoom: 14
-          };
-          var map = new google.maps.Map( document.getElementById(MAP_ELEM_ID), mapOptions);
-          
-          var normalIcon = {
+        var selectedMarkerIndex =0;
+        var map;
+        var normalIcon = {
               url  : "/images/map-icon.png"
           }
           
@@ -24,13 +16,30 @@ define(['async!https://maps.googleapis.com/maps/api/js' , "jquery"], function (G
             url  : "/images/map-icon-highlight.png"
           }
           
+          var markerOptions;
           
-          var markerOptions = {
+          
+          
+        
+        function initialize() {
+          //var loc1 = new google.maps.LatLng(data[0].latitude, data[0].longitude);
+          var mapOptions = {
+            //center: loc1,
+            zoom: 14
+          };
+          map = new google.maps.Map( document.getElementById(MAP_ELEM_ID), mapOptions);
+          
+          markerOptions = {
               icon: normalIcon,
               map:map,
               animation:'DROP'
           }
 
+          
+          
+        }
+        
+        function load(data) {
           var lim = data.length;
           for (var i =0;i<lim;i++) {
               var position = new google.maps.LatLng(data[i].latitude, data[i].longitude);
@@ -50,7 +59,7 @@ define(['async!https://maps.googleapis.com/maps/api/js' , "jquery"], function (G
               thisMarker.index = i;
               markers.push(thisMarker);
           }
-          
+          setPrevNextOnClick(map, markers, data, highlightIcon, normalIcon);
         }
         
         
@@ -60,23 +69,36 @@ define(['async!https://maps.googleapis.com/maps/api/js' , "jquery"], function (G
             unsetMarkers(markers, normalIcon);
             loadWarehouseData(data);
             marker.setIcon(highlightIcon);
-            
+
             function unsetMarkers(markers, normalIcon){
                 var lim = markers.length;
                 for (var i= 0;i<lim;i++){
                     markers[i].setIcon(normalIcon);
                 }
-            }
-            
+            } 
         }
         
         //TODO : move this to select and add a call.
         function setMarkerOnClick(map, marker, markers, data, highlightIcon, normalIcon) {
             google.maps.event.addListener(marker, 'click', function() {
+                selectedMarkerIndex = marker.index;
                 select(marker.index, map, marker, markers, data, highlightIcon, normalIcon);
+            }); 
+        }
+        
+        function setPrevNextOnClick(map, markers, data, highlightIcon, normalIcon) {
+            $("#map-next-result").click(function() {
+                selectedMarkerIndex++;
+                selectedMarkerIndex = selectedMarkerIndex % ( markers.length );
+                select(selectedMarkerIndex, map, markers[selectedMarkerIndex], markers, data[selectedMarkerIndex], highlightIcon, normalIcon)
             });
-            
-            
+            $("#map-prev-result").click(function(){
+                selectedMarkerIndex--;
+                if (selectedMarkerIndex < 0) {
+                    selectedMarkerIndex = markers.length - 1;
+                }
+                select(selectedMarkerIndex, map, markers[selectedMarkerIndex], markers, data[selectedMarkerIndex], highlightIcon, normalIcon)
+            });
             
         }
         
@@ -92,6 +114,9 @@ define(['async!https://maps.googleapis.com/maps/api/js' , "jquery"], function (G
         $(function() {
             initialize();
         });
+        return {
+            load:load
+        }
 
     }
     
