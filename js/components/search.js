3df476acf019fe92ceb07871358b7bf86583ada6
Tom Nuttall <tom.nuttall@weaveability.com>
2015-04-20 09:22:10 +0100

    More search changes and rebuilt JS + CSS

--- js/components/search.js
+++ js/components/search.js
@@ -24,22 +24,40 @@ define(["components/search-results-map", "loom/loom"],function(ResultsMap, Loom)
         }
     });
     
+    
     var loom = new Loom();
     loom.addOnSuccessCallback("search-form", function(response){
         var res = resultsMap.load(response.results);
         if (res) {
             $(".search-result-info-box").fadeIn();
+            $(".continue-links").fadeIn();
+            $(".search-results").fadeIn();
+            $(".info-boxes").fadeOut();
+            $(".testimonials").fadeOut();
+            
             require(["jqueryPlugins/jquery.scrollTo.min"], function(scroll) { 
                 $.scrollTo("#results-area", {duration : 600, offset : -150 });
             });
+            require(["templates/templates"], function(Templates){
+               var templates = new Templates();
+               var template = templates.getTemplate("result-table-row");
+               var lim=response.results.length;
+               for (var i =0 ;i< lim;i++) {
+                   response.results[i].num = i + 1;
+                   response.results[i].href = '/warehouse-profile/' + response.results[i]._id;
+                   var row = template.bind(response.results[i]);
+                   $("#search-results-table tbody ").append(row);
+               }
+            });
+            
+            
         } else {
             alert("no results found for your search");
         }
     });
     loom.addOnErrorCallback("search-form", function(response){
-        alert("posted and got error result");
+        alert("There was an error completing your search");
     });
-    
     
     }
 
