8e1f1f99512ae863a89a986eb79b05ba27f5e973
Marcus Atty <marcus.atty@gmail.com>
2015-07-02 13:23:06 +0100

    About us updates

--- app/routes/static.js
+++ app/routes/static.js
@@ -1,6 +1,5 @@
 var handler = function(app) {
   app.get('/about-us', function (req,res) {
-	  console.log("about us");
 		res.render("about-us",req.data);
 	});
 };
--- server.js
+++ server.js
@@ -76,8 +76,8 @@ require('./app/routes/warehouse-handler')(app);
 require('./app/routes/registration')(app);
 require('./app/routes/search')(app);
 require('./app/routes/dashboard')(app);
-require('./app/routes/error')(app);
 require('./app/routes/static')(app);
+require('./app/routes/error')(app);
 
 console.log("starting node server, you'll see 'listening' on the next line if it was a success:")
 app.listen(port, bind_address, function() {
